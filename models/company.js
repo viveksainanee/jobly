13f93cea071afefd315b902fdf0ed44ab346dafa
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2018-10-24 15:54:26 -0700

    Added some more routes

--- models/company.js
+++ models/company.js
@@ -1,58 +1,91 @@
 const db = require('../db');
+const sqlForPartialUpdate = require('../helpers/partialUpdate');
 
 /** Company class for companies */
 
 class Company {
-  // static async create({ from_username, to_username, body }) {
-  //   try {
-  //     //check if the user exists
-  //     const result = await db.query(
-  //       'SELECT password FROM users WHERE username = $1',
-  //       [to_username]
-  //     );
-  //     let user = result.rows[0];
+  static async create({ handle, name, num_employees, description, logo_url }) {
+    console.log('in create method');
 
-  //     if (user) {
-  //       // insert the message
-  //       const result = await db.query(
-  //         `INSERT INTO messages (from_username, to_username, body, sent_at)
-  //       VALUES ($1, $2, $3, current_timestamp)
-  //       RETURNING id, from_username, to_username, body, sent_at`,
-  //         [from_username, to_username, body]
-  //       );
-  //       return result.rows[0];
-  //     }
-  //   } catch (err) {
-  //     return err;
-  //   }
-  // }
-
-  static async search(handle = '', min = -1, max = 99999999999) {
-    try {
-      console.log('Entering search!! ENHANCE!');
-
+    //check if the company exists
+    const result = await db.query(
+      'SELECT handle FROM companies WHERE handle = $1;',
+      [handle]
+    );
+    let company = result.rows;
+    console.log('company =', company);
+    if (company.length === 0) {
+      console.log('company is 0');
+      // insert the message
       const result = await db.query(
-        `SELECT handle, name 
+        `INSERT INTO companies (handle, name, num_employees, description, logo_url)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING handle, name, num_employees, description, logo_url;`,
+        [handle.toUpperCase(), name, num_employees, description, logo_url]
+      );
+      return result.rows[0];
+    } else {
+      let err = new Error('Company handle already exists');
+      err.status = 400;
+      throw err;
+    }
+  }
+
+  static async search(handle = '', min = -1, max = 999999) {
+    const result = await db.query(
+      `SELECT handle, name
         FROM companies
         WHERE (handle ILIKE $1 OR name ILIKE $1)
         AND num_employees > $2 AND num_employees < $3;`,
-        ['AAPL', '0', '']
-      );
+      ['%' + handle + '%', min, max]
+    );
 
-      // const result = await db.query(
-      //   `SELECT handle, name
-      //   FROM companies
-      //   WHERE (handle ILIKE $1 OR name ILIKE $1)
-      //   AND num_employees > $2 AND num_employees < $3;`,
-      //   [`'%${handle}%'`, min, max]
-      // );
-      console.log('result is ', result);
+    console.log('result is ', result);
 
-      return result.rows;
-    } catch (err) {
-      return err;
+    return { companies: result.rows };
+  }
+
+  static async getByHandle(handle) {
+    const result = await db.query(
+      `SELECT handle, name, num_employees, description, logo_url
+        FROM companies
+        WHERE handle = $1;`,
+      [handle.toUpperCase()]
+    );
+    if (result.rows.length > 0) {
+      return result.rows[0];
+    } else {
+      let err = new Error('Unable to find company');
+      err.status = 400;
+      throw err;
     }
   }
+
+  static async update(handle, name, num_employees, description, logo_url) {
+    let queryObj = sqlForPartialUpdate(
+      'companies',
+      [handle, name, num_employees, description, logo_url],
+      'handle',
+      handle
+    );
+
+    console.log(
+      'query is: ',
+      queryObj.query,
+      '\nvalues are: ',
+      queryObj.values
+    );
+    // const result = await db.query(queryObj.query, queryObj.values);
+    // if (result.rows.length > 0) {
+    //   return result.rows[0];
+    // } else {
+    //   let err = new Error('Unable to find company');
+    //   err.status = 400;
+    //   throw err;
+    // }
+  }
+
+  //sqlForPartialUpdate(table, items, key, id)
 }
 
 module.exports = Company;
--- routes/companies.js
+++ routes/companies.js
@@ -4,19 +4,25 @@ const express = require('express');
 
 const Company = require('../models/company');
 
-router = express.Router();
+const router = new express.Router();
 
 /** Homepage: return handle and name for all companies. 
 It allows for following query string parameters:
 -search
 -min_employees
 -max_employees
+returns
+{companies: [companyData, ...]}
+
+
  
 **/
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     let { search, min_employees, max_employees } = req.query;
+
+    console.log(router);
 
     const companies = await Company.search(
       search,
@@ -25,7 +31,47 @@ router.get('/', async (req, res) => {
     );
     res.json(companies);
   } catch (err) {
-    console.log(err);
+    return next(err);
+  }
+});
+
+/** Creates a new company and returns
+ * {company: companyData}
+ */
+
+router.post('/', async (req, res, next) => {
+  try {
+    console.log('hit post route');
+    const company = await Company.create(req.body);
+    console.log('company is ', company);
+    res.json(company);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** Returns a single company found by handle
+ * {company: companyData}
+ */
+
+router.get('/:handle', async (req, res, next) => {
+  try {
+    let { handle } = req.params;
+
+    const company = await Company.getByHandle(handle);
+    res.json({ company });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.patch('/:handle', async (req, res, next) => {
+  try {
+    let { handle } = req.params;
+
+    const company = await Company.update(req.body);
+    res.json({ company });
+  } catch (err) {
     return next(err);
   }
 });
